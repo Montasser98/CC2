2d159c1736905e6a46adf450de160c95c8e301cd
MONTASSER AL AMSAKI <alamsakimontasser@gmail.com>
2022-06-02 15:58:32 +0100

    Quéstion12

--- app/src/main/java/com/example/cc2/ListeEntreprise.java
+++ app/src/main/java/com/example/cc2/ListeEntreprise.java
@@ -3,12 +3,31 @@ package com.example.cc2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ListeEntreprise extends AppCompatActivity {
+    MyDatabase db;
+    ListView lst;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_liste_entreprise);
+        db = new MyDatabase(this);
+        lst = findViewById(R.id.lst);
+
+        ArrayList<Entreprise> prds = MyDatabase.getAllEntreprise(db.getReadableDatabase());
+
+        ArrayList<String> nomsProds = new ArrayList<>();
+        for(Entreprise pp: prds)
+            nomsProds.add(pp.getID() + " " +
+                     + pp.getCapitale());
+
+        ArrayAdapter ad = new ArrayAdapter(this,android.R.layout.simple_list_item_1,nomsProds);
+
+        lst.setAdapter(ad);
     }
 }
--- app/src/main/java/com/example/cc2/MainActivity.java
+++ app/src/main/java/com/example/cc2/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.cc2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -14,5 +15,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void acces(View view) {
+        Intent i = null;
+        switch (view.getId()){
+            case R.id.b1: i=new Intent(MainActivity.this, AddEntreprise.class); break;
+            case R.id.b2: i=new Intent(MainActivity.this, EditerEntreprise.class); break;
+            case R.id.b3: i=new Intent(MainActivity.this, ListeEntreprise.class); break;
+
+        }
+        startActivity(i);
     }
 }
