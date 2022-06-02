583c7c0624f3c5115758952b3f67b08a600d1686
MONTASSER AL AMSAKI <alamsakimontasser@gmail.com>
2022-06-02 14:20:23 +0100

    Quéstion5

--- app/src/main/java/com/example/cc2/MyDatabase.java
+++ app/src/main/java/com/example/cc2/MyDatabase.java
@@ -35,5 +35,59 @@ public class MyDatabase extends SQLiteOpenHelper {
         sqLiteDatabase.execSQL(sql);
         onCreate(sqLiteDatabase);
     }
+    public static long insert_entreprise (SQLiteDatabase sqLiteDatabase, Entreprise p){
+        ContentValues ct = new ContentValues();
+        ct.put(COL2,p.getRaisonSociale());
+        ct.put(COL3,p.getAdresse());
+        ct.put(COL4,p.getCapitale());
 
+        return sqLiteDatabase.insert(TABLE_NAME,null,ct);
+    }
+
+    public static long update_entrprise(SQLiteDatabase sqLiteDatabase, Entreprise p){
+        ContentValues ct = new ContentValues();
+        ct.put(COL2,p.getRaisonSociale());
+        ct.put(COL3,p.getAdresse());
+        ct.put(COL4,p.getCapitale());
+        return sqLiteDatabase.update(TABLE_NAME,ct,"id="+p.getID(),null);
+    }
+
+    public static long delete_entreprise(SQLiteDatabase sqLiteDatabase, int id){
+        return sqLiteDatabase.delete(TABLE_NAME,"id="+id,null);
+    }
+
+    public static ArrayList<Entreprise> getAllEntreprise(SQLiteDatabase sqLiteDatabase){
+        ArrayList<Entreprise> prds = new ArrayList<>();
+
+        Cursor cur = sqLiteDatabase.rawQuery("SELECT * FROM " + TABLE_NAME,null);
+
+        while(cur.moveToNext()){
+            Entreprise p = new Entreprise();
+            p.setID(cur.getInt(0));
+            p.setRaisonSociale(cur.getString(1));
+            p.setAdresse(cur.getString(2));
+            p.setCapitale(cur.getDouble(3));
+
+            prds.add(p);
+        }
+
+        return prds;
+    }
+
+    public static Entreprise getOneEntreprise(SQLiteDatabase sqLiteDatabase, int id){
+        Entreprise p = null;
+
+        Cursor cur = sqLiteDatabase.rawQuery("SELECT * FROM " + TABLE_NAME + " WHERE id = " + id,null);
+
+        if(cur.moveToNext()){
+            p = new Entreprise();
+            p.setID(cur.getInt(0));
+            p.setRaisonSociale(cur.getString(1));
+            p.setAdresse(cur.getString(2));
+            p.setCapitale(cur.getDouble(3));
+
+        }
+
+        return p;
+    }
 }
